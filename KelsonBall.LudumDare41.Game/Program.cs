b5f01047876e7814f17071e27c53bdbe42a140f1
Kelson Ball <kelson.j.ball@gmail.com>
2018-04-20 17:54:27 -0700

    Add project files.

---
+++ KelsonBall.LudumDare41.Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KelsonBall.LudumDare41.Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
